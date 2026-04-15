c286bdb97f47c208e153cdaa75a202b2b27f3786
GifftyCode <ulokangozi@yahoo.com>
2026-04-15 12:43:13 +0100

    updated codebase

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,23 @@
 
-function App() {
+function ProfileCard ({name, city, aspiration}) {
   return (
     <div>
-      <h1>Name: Uloka Ngozi Gift</h1>
-      <p>City: Abuja</p>
-      <p>Aspiration with React: To build a modern softwares that solve everyday problem</p>
+      <h1>{name}</h1>
+      <p>{city}</p>
+      <p>{aspiration}</p>
+    </div>
+  )
+}
+
+
+
+
+function App () {
+  return (
+    <div>
+      <ProfileCard name="Gift" city="Abuja" aspiration="Billioniar in Dollars and Bitcoin"/>
+<ProfileCard name="Sam" city="LA" aspiration="Most Famous embryologyst"/>
+<ProfileCard name="Chiamak" city="Lagos" aspiration="Wealthiest woman in the world"/>
     </div>
   )
 }
